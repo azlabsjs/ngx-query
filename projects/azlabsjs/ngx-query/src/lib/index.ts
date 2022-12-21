3e1175043ff6f8c91c3652d0abfd3f422205763c
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2022-12-21 20:31:05 +0000

    exported token using the public.api

--- projects/azlabsjs/ngx-query/src/lib/index.ts
+++ projects/azlabsjs/ngx-query/src/lib/index.ts
@@ -1,11 +1,19 @@
 export { ProvidesQuery, QueryDispatch, QueryState } from './decorators';
 export {
   QueryStateLeastParameters,
-  QueryTypeLeastArgumentType, as,
-  returnType, useQuery
+  QueryTypeLeastArgumentType,
+  as,
+  returnType,
+  useQuery
 } from './helpers';
 export { QueryProvider } from './query';
 export { DefaultQueryClient } from './query-client';
 export { QueryModule } from './query.module';
+export {
+  HTTP_CLIENT,
+  HTTP_HOST,
+  HTTP_QUERY_CLIENT,
+  QUERY_MANAGER
+} from './token';
 export { ObserveKeyType } from './types';
 
